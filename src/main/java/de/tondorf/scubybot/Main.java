728a4911115ecaec95bb9fc0293364f005ff543f
Alexander Herold <foxplus@gmx.net>
2011-03-07 19:19:03 +0100

    Initialstand

---
+++ src/main/java/de/tondorf/scubybot/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package de.tondorf.scubybot;
+
+/**
+ *
+ * @author FoXPlus
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        frmHelloSwing.main(args);        
+    }
+
+}
---
+++ src/main/java/de/tondorf/scubybot/ScubyConnector.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package de.tondorf.scubybot;
+
+import java.net.Socket;
+import java.io.IOException;
+import de.tdng2011.game.library.connection.AbstractClient;
+import de.tdng2011.game.library.connection.RelationTypes;
+import de.tdng2011.game.library.World;
+/**
+ *
+ * @author FoXPlus
+ */
+public class ScubyConnector extends AbstractClient{
+
+  private String PlayerName;
+  public String name(){
+      if (PlayerName.length() == 0 ){
+          PlayerName = "DefaultName";
+      }
+      return PlayerName;
+  }
+  public ScubyConnector(final String hostname, final String PlayerName){
+      super(hostname, RelationTypes.Player());
+      this.PlayerName = PlayerName;
+  }
+
+  public void processWorld(World world){
+
+  }
+
+}
